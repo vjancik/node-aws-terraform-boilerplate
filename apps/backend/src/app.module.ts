5f18f406fb6beb94346e97f984f995f9d339b50a
Viktor Jancik <viktor.vix.jancik@gmail.com>
2026-04-05 15:50:12 +0000

    Add backend Dockerfile with minimal logging middleware

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ apps/backend/src/logger.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl } = req;
+    const start = Date.now();
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      this.logger.log(`${method} ${originalUrl} ${statusCode} ${Date.now() - start}ms`);
+    });
+
+    next();
+  }
+}
