534409cdc558797d5876466bfe9931aa021bbd01
Viktor Jancik <viktor.vix.jancik@gmail.com>
2026-04-05 14:58:59 +0000

    Add backend tests with jest + supertest

---
+++ apps/backend/src/app.controller.spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from './app.module';
+
+describe('AppController', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('GET /livez → 200 Ok', () => {
+    return request(app.getHttpServer())
+      .get('/livez')
+      .expect(200)
+      .expect('Ok');
+  });
+
+  it('GET /readyz → 200 Ok', () => {
+    return request(app.getHttpServer())
+      .get('/readyz')
+      .expect(200)
+      .expect('Ok');
+  });
+
+  it('GET / → 200 Ok', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Ok');
+  });
+
+  it('POST / → 200 Ok', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .expect(200)
+      .expect('Ok');
+  });
+
+  it('GET /some/arbitrary/path → 200 Ok', () => {
+    return request(app.getHttpServer())
+      .get('/some/arbitrary/path')
+      .expect(200)
+      .expect('Ok');
+  });
+});
